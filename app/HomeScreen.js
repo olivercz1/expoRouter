df71b3f9ade1a56d2ce0cbeec20224eb88ff82a3
olvercz1 <labelwendell@gmail.com>
2024-09-23 21:12:08 -0300

    confing a homescreen

--- app/HomeScreen.js
+++ app/HomeScreen.js
@@ -1,15 +1,28 @@
 import React from "react";
 import { View,Text,StyleSheet,TouchableOpacity } from "react-native-web";
 import { router } from "expo-router";
+import { StyleSheet } from "react-native";
 
 export default HomeScreen = () => {
     return (
-        <View>
-            <View>
-                <Text> Home</Text>
+        <View style={Styles.container}>
+            <View style={Styles.header}>
+                <Text style={Styles.headertitle}> Home</Text>
             </View>
+
+
+        <Text style={Styles.title}> Bem Vindo!</Text>
+        <TouchableOpacity style={Styles.TouchableOpacity} onPress={( )=>{router.push("/tela2")}}></TouchableOpacity>
         </View>
 
         
     )
 };
+
+const Styles = StyleSheet.create({
+    container : {
+        flex:1,
+        alignItens:"center",
+    },
+
+});
